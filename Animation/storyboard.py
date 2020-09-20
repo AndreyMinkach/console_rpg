0cfc46774e6bdec4324aed49ba272551db9a41cf
PandrPi <nicecrafter0507@gmail.com>
2020-09-21 00:02:09 +0300

    Add Storyboard class which manages all the fields animation Fix Renderer fields names Implement fade_in method in UIBase class. It begins playing an transparency animation which makes a transition of ui object from transparent to opaque Implement fade_out method in UIBase class. It begins playing an transparency animation which makes a transition of ui object from opaque to transparent

---
+++ Animation/storyboard.py
@@ -0,0 +1,28 @@
+from Animation.number_field_animation import NumberFieldAnimation
+
+
+class Storyboard:
+    instance = None
+
+    def __init__(self):
+        self.__class__.instance = self
+        self._animations = []
+        self._animations_to_remove = []
+
+    def begin_animation(self, animation: NumberFieldAnimation):
+        self._animations.append(animation)
+        animation.start_animation()
+
+    def stop_animation(self, animated_object, field_name: str):
+        for a in self._animations:
+            if a.check_source(animated_object, field_name):
+                self._animations_to_remove.append(a)
+
+    def update(self):
+        for a in self._animations:
+            if a.update():
+                self._animations_to_remove.append(a)
+
+        for a in self._animations_to_remove:
+            self._animations.remove(a)
+        self._animations_to_remove.clear()
