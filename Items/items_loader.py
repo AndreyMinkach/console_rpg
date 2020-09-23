1a76568cb5465bdcd132bf47b3dd496899e3269d
PandrPi <nicecrafter0507@gmail.com>
2020-09-23 23:56:47 +0300

    Rename LoadJson to ItemLoader Move majority of code from ItemLoader constructor to load_items method Rename some variables in order them to have more understandable meaning

--- Items/items_loader.py
+++ Items/items_loader.py
@@ -1,19 +1,24 @@
 import json
+import os
 from pydoc import locate
 
 
-class LoadJson:
+class ItemLoader:
     ITEM_TYPE_LIST = ['Items.Weapons.weapon.Weapon', 'Items.Outfits.outfit.Outfit']
 
     def __init__(self):
-        static_folder_path = 'Static/Items/'
-        json_files = ["weapons.json", "outfits.json"]
+        self._items = {}
+        self.load_items()
+
+    def load_items(self):
+        folder_path = 'Static/Items/'
+        file_list = [os.path.join(folder_path, f) for f in os.listdir(folder_path) if f.endswith('.json')]
         items = {}
-        for json_file in json_files:
-            with open(static_folder_path + json_file) as f:
+        for file_path in file_list:
+            with open(file_path, 'r') as f:
                 temp_json = json.load(f)
-            for value_item in temp_json:
-                items[value_item['id']] = value_item
+                for value_item in temp_json:
+                    items[value_item['id']] = value_item
         self._items = items
 
     def get_item_by_id(self, item_id: int):
@@ -22,12 +27,12 @@ class LoadJson:
 
         temp_dict = self._items[item_id]
         item_type = temp_dict['item_type']
-        for i in LoadJson.ITEM_TYPE_LIST:
+        for i in ItemLoader.ITEM_TYPE_LIST:
             if item_type in i:
                 item_type = i
                 break
 
-        item = locate(item_type)
-        instance_item = item()
-        instance_item.__dict__.update(temp_dict)
-        return instance_item
+        located_item_class = locate(item_type)
+        item_instance = located_item_class()  # call constructor of class to get its instance
+        item_instance.__dict__.update(temp_dict)
+        return item_instance
