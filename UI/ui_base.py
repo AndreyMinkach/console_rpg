4bb38f6a2e66aa359adfccec6fa03fa656771eac
PandrPi <nicecrafter0507@gmail.com>
2020-09-28 23:02:43 +0300

    Some little changes

--- UI/ui_base.py
+++ UI/ui_base.py
@@ -1,14 +1,13 @@
-import types
-
 import pyglet
 from pyglet.sprite import Sprite
 
+from Helpers.color_helper import ColorHelper
 from Helpers.location_helper import Vector2
 
 
 class UIBase(Sprite):
     def __init__(self, position: Vector2, size: Vector2):
-        image = pyglet.image.SolidColorImagePattern((255, 255, 255, 255)).create_image(size.x, size.y)
+        image = pyglet.image.SolidColorImagePattern(ColorHelper.WHITE).create_image(size.x, size.y)
         image.height = size.x
         image.width = size.y
         super().__init__(image, x=position.x, y=position.y)
