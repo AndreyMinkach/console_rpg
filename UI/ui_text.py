702d1240b861d7330c2ef055f44520737b28699a
AndreyMinkach <luckydron066@gmail.com>
2020-09-30 15:03:41 +0300

    Fix

--- UI/ui_text.py
+++ UI/ui_text.py
@@ -14,9 +14,7 @@ class UIText(UIBase):
     def __init__(self, text: str, position: Vector2, size: Vector2,
                  color: Tuple[int, int, int, int] = ColorHelper.PINK):
         font.add_file('Static/Fonts/DisposableDroidBB.ttf')
-        ZukaDoodle = font.load('DisposableDroidBB.ttf', 16)
-        # add font (not ended)
-        text = Label(text, font_name="Disposable Droid B B", color=color, x=position.x, y=position.y, width=size.x, height=size.y, multiline=True)
+        text = Label(text, color=color, x=position.x, y=position.y, width=size.x, height=size.y, multiline=True)
         self.children = []
         self.children.append(text)
 
--- main.py
+++ main.py
@@ -38,12 +38,8 @@ if __name__ == '__main__':
     window = MyWindow(configs.WINDOW_WIDTH, configs.WINDOW_HEIGHT, caption=configs.WINDOW_TITLE, resizable=True,
                       vsync=True)
 
-    temp_ui_text1 = UIText("Updates the callingpdates the calling abject and all its  Updates the calling abject "
-                           "and all itspdates the calling abject and all its  Updates the calling abject and all "
-                           "itspdates the calling abject and all its  Updates the calling abject and all its abject "
-                           "and all its  Updates the calling abject and all its ",
+    temp_ui_text1 = UIText("Updates the callingpdates the calling abject ",
                            Vector2(400, 200), Vector2(200, 0))
-
 
     temp_base1 = UIBase(Vector2(100, 100), Vector2(300, 200))
     renderer.add_ui_object(temp_ui_text1)
